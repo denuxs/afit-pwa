a6142ff84f2e6dc8104ed4abb9365a3e4bdcb781
Denis López <denuxs@gmail.com>
2025-04-24 18:07:00 -0600

    refactor workouts list

--- src/app/pages/workouts/workouts.component.ts
+++ src/app/pages/workouts/workouts.component.ts
@@ -1,15 +1,16 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgClass } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 import { User, Workout } from 'app/domain';
 import { UserService, WorkoutService } from 'app/services';
-import { WorkoutItemComponent } from './workout-item/workout-item.component';
+import { SkeletonComponent } from 'app/components/skeleton/skeleton.component';
 
 @Component({
   selector: 'app-workouts',
   standalone: true,
-  imports: [AsyncPipe, WorkoutItemComponent],
+  imports: [AsyncPipe, NgClass, RouterLink, SkeletonComponent],
   templateUrl: './workouts.component.html',
   styleUrl: './workouts.component.scss',
 })
@@ -19,7 +20,36 @@ export class WorkoutsComponent implements OnInit {
 
   workouts$!: Observable<Workout[]>;
 
+  // daysOfWeek = [
+  //   'Lunes',
+  //   'Martes',
+  //   'Miercoles',
+  //   'Jueves',
+  //   'Viernes',
+  //   'Sabado',
+  //   'Domingo',
+  // ];
+
+  daysOfWeek = ['Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab', 'Dom'];
+
+  colors = [
+    'from-blue to-blue-500',
+    'from-pink-400 to-pink-500',
+    'from-rose-400 to-rose-500',
+    'from-amber-500 to-pink-500',
+    'from-emerald-500 to-green-900',
+    'from-cyan-400 to-cyan-500',
+    'from-rose-400 to-red-500',
+    'from-red-500 to-orange-500',
+  ];
+  gradient = '';
+
   ngOnInit(): void {
+    const randomNumber = Math.floor(Math.random() * 7) + 1;
+    const randomColor = this.colors[randomNumber];
+
+    this.gradient = randomColor;
+
     this._userService.user$.subscribe((user) => {
       this.getWorkouts(user);
     });
@@ -32,5 +62,14 @@ export class WorkoutsComponent implements OnInit {
       is_active: true,
     };
     this.workouts$ = this._workoutService.fetchWorkouts(params);
+  }
+
+  getDayOfWeek(index: number): string {
+    return this.daysOfWeek[index - 1];
+  }
+
+  getMuscles(exercises: any) {
+    const data = exercises.map((e: any) => e.exercise.muscle.name);
+    return [...new Set(data)];
   }
 }
