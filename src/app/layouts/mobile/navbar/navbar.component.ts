63da7a05c2dc548fa8cdb81a7a0e554662747181
Denis López <denuxs@gmail.com>
2025-04-24 18:32:41 -0600

    test check network

--- src/app/layouts/mobile/navbar/navbar.component.ts
+++ src/app/layouts/mobile/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { NetworkService } from 'app/services/network.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,8 +9,17 @@ import { Meta, Title } from '@angular/platform-browser';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   private readonly _titleService = inject(Title);
+  private readonly _networkService = inject(NetworkService);
+
+  isOnline = false;
+
+  ngOnInit(): void {
+    this._networkService.isOnline$.subscribe((isOnline) => {
+      this.isOnline = isOnline;
+    });
+  }
 
   get title() {
     return this._titleService.getTitle();
