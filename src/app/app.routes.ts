39a71697dfb7dae8f9dbf4512d8baf1576f8f069
Denis López <denuxs@gmail.com>
2025-06-13 15:22:04 -0600

    refactor routes file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,89 +1,65 @@
 import { Routes } from '@angular/router';
 
-import { CoachTabsComponent } from './layouts/coach-tabs/coach-tabs.component';
-import { ClientTabsComponent } from './layouts/client-tabs/client-tabs.component';
-
-import { clientGuard } from './core/guards/client.guard';
-import { coachGuard } from './core/guards/coach.guard';
+import { noAuthGuard } from './core/guards/no-auth.guard';
+import { authGuard } from './core/guards/auth.guard';
+import { LayoutTabsComponent } from './layouts/layout-tabs/layout-tabs.component';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/profile' },
+  { path: '', pathMatch: 'full', redirectTo: 'profile' },
   {
     path: 'signin',
+    canActivate: [noAuthGuard],
     loadComponent: () =>
       import('./pages/login/login.component').then((c) => c.LoginComponent),
   },
   {
-    path: 'routines',
-    title: 'Rutinas',
-    loadChildren: () => import('./pages/client/routines/routines.routes'),
-  },
-  {
-    path: 'exercises',
-    title: 'Ejercicios',
-    loadChildren: () => import('./pages/client/exercises/exercises.routes'),
-  },
-  {
-    path: 'coach',
-    component: CoachTabsComponent,
-    canActivate: [coachGuard],
+    path: '',
+    component: LayoutTabsComponent,
+    canActivate: [authGuard],
+    // canActivateChild: [authGuard],
     children: [
+      {
+        path: 'profile',
+        title: 'Perfil',
+        loadComponent: () =>
+          import('./pages/profile/profile.component').then(
+            (c) => c.ProfileComponent,
+          ),
+      },
       {
         path: 'clients',
         title: 'Clientes',
+        // canActivate: [authGuard],
         loadComponent: () =>
-          import('./pages/coach/clients/clients.component').then(
+          import('./pages/clients/clients.component').then(
             (c) => c.ClientsComponent,
           ),
       },
       {
-        path: 'profile',
-        title: 'Perfil',
-        loadComponent: () =>
-          import('./pages/coach/coach-profile/coach-profile.component').then(
-            (c) => c.CoachProfileComponent,
-          ),
-      },
-    ],
-  },
-  {
-    path: '',
-    component: ClientTabsComponent,
-    canActivate: [clientGuard],
-    children: [
-      {
-        path: 'profile',
-        title: 'Perfil',
-        loadComponent: () =>
-          import('./pages/client/profile/profile.component').then(
-            (c) => c.ProfileComponent,
-          ),
-      },
-
-      {
         path: 'workouts',
         title: 'Rutinas',
-        loadChildren: () => import('./pages/client/workouts/workouts.routes'),
+        loadChildren: () => import('./pages/workouts/workouts.routes'),
+      },
+      {
+        path: 'routines',
+        title: 'Rutinas',
+        loadChildren: () => import('./pages/routines/routines.routes'),
+      },
+      {
+        path: 'exercises',
+        title: 'Ejercicios',
+        loadChildren: () => import('./pages/exercises/exercises.routes'),
       },
       {
         path: 'measures',
         title: 'Medidas',
-        loadChildren: () => import('./pages/client/measures/measures.routes'),
+        loadChildren: () => import('./pages/measures/measures.routes'),
       },
       {
         path: 'about',
         title: 'Acerca',
         loadComponent: () =>
-          import('./pages/client/about/about.component').then(
-            (c) => c.AboutComponent,
-          ),
-      },
-      {
-        path: 'notifications',
-        loadComponent: () =>
-          import('./pages/client/notifications/notifications.component').then(
-            (c) => c.NotificationsComponent,
-          ),
+          import('./pages/about/about.component').then((c) => c.AboutComponent),
       },
     ],
   },
